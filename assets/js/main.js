9a63b29fc04fedcc5942d8a3d1257aee05e2f1da
Mohamed5amy <you@example.com"you@example.com>
2022-02-11 02:59:19 +0200

    animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,12 @@ menuLi.forEach((li)=> {
     })
 }) 
 // End Menu 
+// The form 
+var plan1B = document.querySelector(".plan1")
+var plan2B = document.querySelector(".plan2")
+plan1B.addEventListener("click", ()=>{
+    window.open("https://mail.google.com/mail/u/0/?fs=1&to=mohamedsamy582002@gmail.com&su=Hi I'm Coming From The Website about (خدمات محاسب أونلاين) plan &body=Enter your message here &tf=cm")
+})
+plan2B.addEventListener("click", ()=>{
+    window.open("https://mail.google.com/mail/u/0/?fs=1&to=mohamedsamy582002@gmail.com&su=Hi I'm Coming From The Website about (مؤسسات ناشئة) plan &body=Enter your message here &tf=cm")
+})
